a7bbdb1093c13d782be16c4e6ff050a39d77c753
Mohamad Radisha <pr0ph0z23@gmail.com>
2017-11-10 20:23:12 +0700

    Gabut

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+echo "Masukkan kalimat gabut lo wkowkowkow = ";
+$string = str_split(trim(fgets(STDIN)));
+	for ($i=0; $i <count($string) ; $i++) {
+		for ($a=0; $a <= $i; $a++) { 
+			echo $string[$a];
+		}
+		if($i<=count($string)-1) {
+			echo "\n";
+		}
+	}
+	for ($b=count($string)-1; $b >= 0 ; $b--) { 
+		for ($c=0; $c <= $b; $c++) { 
+			echo $string[$c];
+		}
+		echo "\n";
+	}
+?>
